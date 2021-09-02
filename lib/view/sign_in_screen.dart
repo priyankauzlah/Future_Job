5a37e30f171cf13a3c8d2ac13c3865b4cb362bc5
priyanka uzlah <priyankauzlah@gmail.com>
2021-09-02 15:09:07 +0700

    [HW] sign in button and new account button

--- lib/view/sign_in_screen.dart
+++ lib/view/sign_in_screen.dart
@@ -20,96 +20,132 @@ class _SigninScreenState extends State<SigninScreen> {
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Sign In',
-                style: greyTextStyle.copyWith(fontSize: 16.0),
-              ),
-              SizedBox(height: 4.0),
-              Text(
-                'Build Your Career',
-                style: blackTextStyle.copyWith(
-                    fontSize: 24.0, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              Center(
-                child: Image.asset('assets/images/image_sign_in.png',
-                    width: 270.0),
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              Text(
-                'Email',
-                style: greyTextStyle.copyWith(fontSize: 16.0),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextFormField(
-                cursorColor: primaryColor,
-                controller: emailController,
-                decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 26.0, vertical: 20.0),
-                    fillColor: formColor,
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Sign In',
+                  style: greyTextStyle.copyWith(fontSize: 16.0),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  'Build Your Career',
+                  style: blackTextStyle.copyWith(
+                      fontSize: 24.0, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Center(
+                  child: Image.asset('assets/images/image_sign_in.png',
+                      width: 270.0),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Text(
+                  'Email',
+                  style: greyTextStyle.copyWith(fontSize: 16.0),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  cursorColor: primaryColor,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 26.0, vertical: 20.0),
+                      fillColor: formColor,
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide.none),
+                      focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20.0),
-                        borderSide: BorderSide.none),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      borderSide: BorderSide(
-                          color: EmailValidator.validate(emailController.text)
-                              ? primaryColor
-                              : Colors.red),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      borderSide: BorderSide(
-                          color: EmailValidator.validate(emailController.text)
-                              ? primaryColor
-                              : Colors.red),
-                    ),
-                    hintText: ''),
-                style: EmailValidator.validate(emailController.text)
-                    ? purpleTextStyle
-                    : redTextStyle,
-              ),
-              Text(
-                'Password',
-                style: greyTextStyle.copyWith(fontSize: 16.0),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextFormField(
-                obscureText: true,
-                cursorColor: primaryColor,
-                controller: passwordController,
-                decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 26.0, vertical: 20.0),
-                    fillColor: formColor,
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: EmailValidator.validate(emailController.text)
+                                ? primaryColor
+                                : Colors.red),
+                      ),
+                      border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20.0),
-                        borderSide: BorderSide.none),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      borderSide: BorderSide(color: primaryColor),
+                        borderSide: BorderSide(
+                            color: EmailValidator.validate(emailController.text)
+                                ? primaryColor
+                                : Colors.red),
+                      ),
+                      hintText: ''),
+                  style: EmailValidator.validate(emailController.text)
+                      ? purpleTextStyle
+                      : redTextStyle,
+                ),
+                Text(
+                  'Password',
+                  style: greyTextStyle.copyWith(fontSize: 16.0),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  cursorColor: primaryColor,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 26.0, vertical: 20.0),
+                      fillColor: formColor,
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide.none),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: primaryColor),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      hintText: ''),
+                  style: purpleTextStyle,
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 45.0,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(65.0),
+                      ),
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                    child: Text(
+                      'Sign In',
+                      style:
+                      whiteTextStyle.copyWith(fontWeight: FontWeight.w600),
                     ),
-                    hintText: ''),
-                style: purpleTextStyle,
-              ),
-            ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Create New Account',
+                        style: greyTextStyle.copyWith(
+                            fontWeight: FontWeight.w400)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
